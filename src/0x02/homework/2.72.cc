80773df0eaf9f56ae88544a5f16beb044cf5cab8
algebra2k <jackson.cloudnative@gmail.com>
2021-07-08 19:01:53 +0800

    add 2.67 2.72 solution

---
+++ src/0x02/homework/2.72.cc
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+void copy_int(int val, void* buf, int maxbytes) {
+  if (maxbytes < sizeof(int)) {
+    return;
+  }
+
+  if (maxbytes - sizeof(int) >= 0) {
+    std::memcpy(buf, &val, sizeof(int));
+  };
+}
+
+void test_copy_int() {
+  int val = 0x7FFFFFFF;
+  char enough_buf[sizeof(int)];
+  copy_int(val, enough_buf, sizeof(int));
+  assert(*reinterpret_cast<int*>(enough_buf) == val);
+
+  char short_buf[sizeof(short)];
+  copy_int(val, short_buf, sizeof(short));
+  assert(*reinterpret_cast<int*>(short_buf) != val);
+
+  char zero_buf[sizeof(short)];
+  copy_int(val, zero_buf, sizeof(short));
+  assert(*reinterpret_cast<int*>(zero_buf) != val);
+}
+
+int main() { test_copy_int(); }
